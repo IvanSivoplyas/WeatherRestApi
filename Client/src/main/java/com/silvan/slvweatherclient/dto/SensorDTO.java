783fa39c1181175fac44b22476b3c3b6fb1f744e
Ivan Sivoplyas <124461656+IvanSivoplyas@users.noreply.github.com>
2025-02-05 15:39:29 +0400

    Add files via upload

---
+++ Client/src/main/java/com/silvan/slvweatherclient/dto/SensorDTO.java
@@ -0,0 +1,12 @@
+package com.silvan.slvweatherclient.dto;
+public class SensorDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
